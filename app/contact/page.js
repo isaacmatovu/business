252cd5c197b5313be61000026470389a492e466a
isaacmatovu <matovuisaac646@gmail.com>
2025-05-28 13:39:08 +0300

    added contact page

---
+++ app/contact/page.js
@@ -0,0 +1,11 @@
+import ContactInformation from "../components/contact/contactForm";
+import Hero from "../components/contact/hero";
+
+export default function page() {
+  return (
+    <div>
+      <Hero />
+      <ContactInformation />
+    </div>
+  );
+}
