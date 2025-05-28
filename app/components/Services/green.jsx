a7de19feed1e650421557e15475f2ff8366f11c0
isaacmatovu <matovuisaac646@gmail.com>
2025-05-28 13:40:28 +0300

    added services component

---
+++ app/components/Services/green.jsx
@@ -0,0 +1,12 @@
+export default function GreenCard() {
+  return (
+    <div className="flex justify-center text-center relative z-20 -mt-16 sm:-mt-20">
+      <div className=" bg-gradient-to-b from-emerald-400 to-teal-600 w-[70%] h-56 shadow-2xl p-6">
+        <h1>Our projects</h1>
+        <p className="text-xl sm:text-6xl font-bold text-white">
+          We will help your business growing
+        </p>
+      </div>
+    </div>
+  );
+}
