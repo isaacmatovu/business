f311a386adb455fcf30dcc063cfd938c21cc5b32
isaacmatovu <matovuisaac646@gmail.com>
2025-05-28 13:43:32 +0300

    added inter font

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/common/Header";
+import Footer from "./components/common/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400"],
 });
 
 export const metadata = {
@@ -19,10 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
+        <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
