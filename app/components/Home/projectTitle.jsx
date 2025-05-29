d9e6156e209b963b813460e45fac4ed55c81354a
isaacmatovu <matovuisaac646@gmail.com>
2025-05-29 15:15:08 +0300

    adjusted title font

--- app/components/Home/projectTitle.jsx
+++ app/components/Home/projectTitle.jsx
@@ -18,20 +18,6 @@ export default function ProjectsSection() {
           Let's check some of our perfect projects.
         </p>
       </div>
-
-      {/* Chat button */}
-      <div className="absolute bottom-8 left-8">
-        <button className="bg-[#ffc107] w-14 h-14 rounded-full flex items-center justify-center shadow-md">
-          <MessageCircle className="w-6 h-6 text-gray-800" />
-        </button>
-      </div>
-
-      {/* Back to top button */}
-      <div className="absolute bottom-8 right-8">
-        <button className="bg-[#4ecca3] w-12 h-12 rounded-full flex items-center justify-center shadow-md">
-          <ArrowUp className="w-5 h-5 text-white" />
-        </button>
-      </div>
     </section>
   );
 }
